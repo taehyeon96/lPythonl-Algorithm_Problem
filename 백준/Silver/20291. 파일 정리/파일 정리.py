92a33e36f202fdb55d05f5b45c8a3f5887bb422a
TaeHyeon Kwon <61690289+taehyeon96@users.noreply.github.com>
2022-07-13 19:20:24 +0900

    [Silver III] Title: 파일 정리, Time: 2112 ms, Memory: 43640 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.py
+++ 백준/Silver/20291. 파일 정리/파일 정리.py
@@ -1,12 +1,16 @@
-from collections import Counter
+# 카운터 안쓰고 딕셔너리로만 푼 방법
+n = int(input())
 
-arr = [input() for i in range(int(input()))]
-
-ans = []
-for i in arr:
-    _, key = i.split('.')
-    ans.append(key)
+file = dict()
+for _ in range(n):
+    extend = (input().split('.'))[1]  # (중요) 입력받자마자 바로 확장자만 저장
     
-c = sorted(dict(Counter(ans)).items())
-for i in c:
-    print(i[0], i[1])
+    if not extend in file:
+        file[extend] = 1               # 딕셔너리는 해당 키값이 idx임
+    else:
+        file[extend] += 1
+
+sort_file = sorted(file.items())       # 키값(item값)을 기준으로 정렬
+
+for key, val in sort_file:             # 딕셔너리 가져올 때 반드시!!
+    print(key.rstrip(), val)
