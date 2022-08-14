af39d61bab4069e6dd78e6b953d6ca30c0741276
TaeHyeon Kwon <61690289+taehyeon96@users.noreply.github.com>
2022-08-14 16:31:27 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 180 ms, Memory: 35328 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -1,5 +1,5 @@
-# 방법 2 : bisect를 사용하고, 찾고자 하는 값의 앞에 데이터를 넣어줌
-from bisect import bisect
+# 방법 3 : bisect_left를 사용해서, 찾고자 하는 값의 lower_bound를 검색
+from bisect import bisect_left
 
 for _ in range(int(input())):
     N, M = map(int, input().split())
@@ -7,5 +7,5 @@ for _ in range(int(input())):
     B = sorted(list(map(int, input().split())))
     cnt = 0
     for a in A:
-        cnt += (bisect(B, a-1))  # a보다 하나 작은 값의 인덱스를 찾아서 그 인덱스를++ 
+        cnt += (bisect_left(B, a))  # a보다 하나 작은 값의 인덱스를 찾아서 그 인덱스를++ 
     print(cnt)
